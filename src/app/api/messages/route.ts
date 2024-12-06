e35e19ceefdc136d500f282f04fa68f45c5450ef
Justin Tefteller <justin.tefteller@wpengine.com>
2024-12-06 00:26:21 -0600

    fix handler

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,10 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { contactMessage } from '@/lib/validation'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST')
     return res.status(405).json({ error: 'Method not allowed' })
   let message
